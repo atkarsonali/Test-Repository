50ba96f3dbe0ebb425f60ad9b39325443369ea23
Sonali <sonali.atkar@kratinsolutions.com>
2022-02-06 23:31:53 -0800

    test added

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -4,10 +4,7 @@ describe('first test',()=>{
         expect(true).to.equal(true)
         expect(true).to.equal(true)
     })
-    it('verify visit',()=>{
-        cy.visit('http://localhost:8020', { responseTimeout: 2000000 , timeout: 1000000 })
-        cy.wait(20000)
-   })
+ 
 
    
 })
